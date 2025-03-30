41141c77ccff2b25cd67d446c3259489a5fe5131
lamorasjr <lamorasjr@gmail.com>
2025-03-30 16:07:44 +0200

    updated seeds and dbt_project

--- dbt_adventure_works/models/staging/stg_product_category.sql
+++
@@ -1,4 +0,0 @@
-select
-	productcategoryid as product_category_id,
-	name as category_name
-from {{ ref('product_category') }}
--- dbt_adventure_works/models/staging/stg_product_subcategory.sql
+++
@@ -1,5 +0,0 @@
-select
-	productsubcategoryid as product_subcategory_id,
-	productcategoryid as product_category_id,
-	name as subcategory_name
-from {{ ref('product_subcategory') }}
