f0bed6148bdca7b136a633e15387e2d907f3d128
lamorasjr <lamorasjr@gmail.com>
2025-03-30 15:05:58 +0200

    added schema and tests properties for dim_products.sql

--- dbt_adventure_works/models/marts/dim_products.sql
+++ dbt_adventure_works/models/marts/dim_products.sql
@@ -1,20 +1,21 @@
-with stg_product as (
+with product as (
 	select *
 	from {{ ref('stg_product' )}}
 ),
 
-stg_product_subcategory as (
+product_subcategory as (
 	select *
 	from {{ ref('stg_product_subcategory' )}}
 ),
 
-stg_product_category as (
+product_category as (
 	select *
 	from {{ ref('stg_product_category' )}}
 ),
 
 tb_final as (
 	select
+		{{ dbt_utils.generate_surrogate_key(['t1.product_id'])  }} as product_key,
 		t1.product_id,
 		t1.product_name,
 		t1.product_class,
@@ -26,10 +27,10 @@ tb_final as (
 		t2.subcategory_name,
 		t3.product_category_id,
 		t3.category_name
-	from stg_product as t1
-		left join stg_product_subcategory as t2
+	from product as t1
+		left join product_subcategory as t2
 			on t1.product_subcategory_id = t2.product_subcategory_id
-		left join stg_product_category as t3
+		left join product_category as t3
 			on t2.product_category_id = t3.product_category_id
 )
 
